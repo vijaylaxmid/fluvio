ed03819dd13063cac49715c9a478350f0b3f637f
Nick Mosher <nicholastmosher@gmail.com>
2020-11-18 10:19:20 -0500

    Change error to tuple enums for easier destructuring

--- src/cli/src/error/mod.rs
+++ src/cli/src/error/mod.rs
@@ -1,5 +1,7 @@
 use std::io::Error as IoError;
 
+mod user_error;
+
 use fluvio::FluvioError;
 use fluvio_cluster::{ClusterError, CheckError};
 
@@ -8,52 +10,25 @@ pub type Result<T> = std::result::Result<T, CliError>;
 #[derive(thiserror::Error, Debug)]
 pub enum CliError {
     #[error(transparent)]
-    IoError {
-        #[from]
-        source: IoError,
-    },
+    IoError(#[from] IoError),
     #[error("Fluvio client error")]
-    ClientError {
-        #[from]
-        source: FluvioError,
-    },
+    ClientError(#[from] FluvioError),
     #[error("Fluvio cluster error")]
-    ClusterError {
-        #[from]
-        source: ClusterError,
-    },
+    ClusterError(#[from] ClusterError),
     #[error("Fluvio cluster pre install check error")]
-    CheckError {
-        #[from]
-        source: CheckError,
-    },
+    CheckError(#[from] CheckError),
     #[error("Kubernetes config error")]
-    K8ConfigError {
-        #[from]
-        source: k8_config::ConfigError,
-    },
+    K8ConfigError(#[from] k8_config::ConfigError),
     #[error("Kubernetes client error")]
-    K8ClientError {
-        #[from]
-        source: k8_client::ClientError,
-    },
+    K8ClientError(#[from] k8_client::ClientError),
+    #[error("Package index error")]
+    IndexError(#[from] fluvio_index::Error),
+    #[error("Error finding executable")]
+    WhichError(#[from] which::Error),
+    #[error(transparent)]
+    HttpError(#[from] HttpError),
     #[error("Invalid argument: {0}")]
     InvalidArg(String),
-    #[error("Package index error")]
-    IndexError {
-        #[from]
-        source: fluvio_index::Error,
-    },
-    #[error("Error finding executable")]
-    WhichError {
-        #[from]
-        source: which::Error,
-    },
-    #[error(transparent)]
-    HttpError {
-        #[from]
-        source: HttpError,
-    },
     #[error("Unknown error: {0}")]
     Other(String),
 }
@@ -65,10 +40,8 @@ pub struct HttpError {
 }
 
 impl From<http_types::Error> for CliError {
-    fn from(e: http_types::Error) -> Self {
-        CliError::HttpError {
-            source: HttpError { inner: e },
-        }
+    fn from(inner: http_types::Error) -> Self {
+        Self::HttpError(HttpError { inner })
     }
 }
 
